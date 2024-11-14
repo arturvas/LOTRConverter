5a314bdfaeb19f14fd9362ecd2848998ea43affc
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-11-14 16:08:21 -0400

    add extrated subview

--- LOTRConverter17/ExchangeRate.swift
+++ LOTRConverter17/ExchangeRate.swift
@@ -5,4 +5,34 @@
 //  Created by Artur Vasconcelos on 14/11/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ExchangeRate: View {
+    let leftImage: ImageResource
+    let text: String
+    let rightImage: ImageResource
+    
+    
+    var body: some View {
+        HStack{
+//            Left currency image
+            Image(leftImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 33)
+            
+//             Exchange rate text
+            Text(text)
+            
+//             Right currency image
+            Image(rightImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 33)
+        }
+    }
+}
+
+#Preview {
+    ExchangeRate(leftImage: .silverpiece, text: "1 Silver Piece = 4 Silver Pennies", rightImage: .silverpenny)
+}
