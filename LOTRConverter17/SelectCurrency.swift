2538b12f536c2c7363f6aa4b8bbfb4a8aee44641
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-11-15 00:52:14 -0400

    make select currency view

--- LOTRConverter17/SelectCurrency.swift
+++ LOTRConverter17/SelectCurrency.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct SelectCurrency: View {
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+//            Parchment background image
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            
+            VStack{
+//                Text
+                Text("Select the currency you are starting with:")
+                    .fontWeight(.bold)
+                
+//                Currency icons
+                CurrencyIcon(currencyImage: .copperpenny, currencyName: "Copper Peny")
+                
+                
+//                Text
+                Text("Select the currency you would like to convert to:")
+                    .fontWeight(.bold)
+                
+//                Currency icons
+                
+//                Done button
+                Button("Done"){
+                    dismiss()
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown)
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(.white)
+            }
+            .padding()
+            .multilineTextAlignment(.center)
+        }
     }
 }
 
