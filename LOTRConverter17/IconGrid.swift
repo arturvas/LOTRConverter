16067a906477984b49efa8c720f69cf1b620311f
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-11-27 09:26:18 -0400

    add CurrencyIcon view

--- LOTRConverter17/IconGrid.swift
+++ LOTRConverter17/IconGrid.swift
@@ -7,66 +7,34 @@
 
 import SwiftUI
 
-struct SelectCurrency: View {
-    @Environment(\.dismiss) var dismiss
-    @State var selectedCurrency: Currency?
-    
+//selected icons grid
+struct IconGrid: View {
+    @State var selectedCurrency: Currency
+
     var body: some View {
-        ZStack{
-//            Parchment background image
-            Image(.parchment)
-                .resizable()
-                .ignoresSafeArea()
-                .background(.brown)
-            
-            VStack{
-//                Text
-                Text("Select the currency you are starting with:")
-                    .fontWeight(.bold)
-                
-//                Currency icons
-                LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
-                    ForEach(Currency.allCases) { currency in
-                        if selectedCurrency == currency {
-                            CurrencyIcon(currencyImage: currency.image, currencyName: currency.name)
-                                .shadow(color: .black, radius: 10)
-                                .overlay {
-                                    RoundedRectangle(cornerRadius: 25)
-                                        .stroke(lineWidth: 3)
-                                        .opacity(0.5)
-                                }
-                            
-                        } else {
-                            CurrencyIcon(currencyImage: currency.image, currencyName: currency.name)
-                                .onTapGesture {
-                                    selectedCurrency = currency
-                                }
+//        Currency icons
+        LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
+            ForEach(Currency.allCases) { currency in
+                if selectedCurrency == currency {
+                    CurrencyIcon(currencyImage: currency.image, currencyName: currency.name)
+                        .shadow(color: .black, radius: 10)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 25)
+                                .stroke(lineWidth: 3)
+                                .opacity(0.5)
                         }
-                    }
+                } else {
+                    CurrencyIcon(currencyImage: currency.image, currencyName: currency.name)
+                        .onTapGesture {
+                            selectedCurrency = currency
+                        }
                 }
-                
-//                Text
-                Text("Select the currency you would like to convert to:")
-                    .fontWeight(.bold)
-                
-//                Currency icons
-                
-//                Done button
-                Button("Done"){
-                    dismiss()
-                }
-                .buttonStyle(.borderedProminent)
-                .tint(.brown)
-                .font(.largeTitle)
-                .padding()
-                .foregroundStyle(.white)
             }
-            .padding()
-            .multilineTextAlignment(.center)
         }
+        .padding()
     }
 }
 
 #Preview {
-    SelectCurrency(selectedCurrency: .silverPiece)
+    IconGrid(selectedCurrency: .silverPiece)
 }
